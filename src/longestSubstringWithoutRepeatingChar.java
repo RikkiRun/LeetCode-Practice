06214a7f4489ed1171b5b99f1dc7e3134e987d42
RikkiRun <31293953+RikkiRun@users.noreply.github.com>
2018-08-31 19:05:17 -0600

    add

---
+++ src/longestSubstringWithoutRepeatingChar.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() == 0 || s == null) {
+            return 0;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int i = 0, j = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                j = Math.max(j, map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i), i);
+            res = Math.max(res, i - j + 1);
+        }
+        return res;
+    }
+}
